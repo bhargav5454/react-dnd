b32c4c9a64070e3212c4a15464d8b8abcce9a4dc
bhargav5454 <ultracubes7.com>
2024-11-09 12:13:57 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,56 +24,74 @@ const App = () => {
   };
   const [columns, setColumns] = useState(initialColumns);
   const onDragEnd = ({ source, destination }) => {
-       // Make sure we have a valid destination
-       if (destination === undefined || destination === null) return null
-
-       // If the source and destination columns are the same
-       // AND if the index is the same, the item isn't moving
-       if (
-         source.droppableId === destination.droppableId &&
-         destination.index === source.index
-       )
-         return null
-   
-   
-       // Set start and end variables
-       const start = columns[source.droppableId]
-       const end = columns[destination.droppableId]
+    // Make sure we have a valid destination
+    if (destination === undefined || destination === null) return null
+ 
+    // Make sure we're actually moving the item
+    if (
+      source.droppableId === destination.droppableId &&
+      destination.index === source.index
+    )
+      return null
+ 
+    // Set start and end variables
+    const start = columns[source.droppableId]
+    const end = columns[destination.droppableId]
+ 
     // If start is the same as end, we're in the same column
     if (start === end) {
-      /* ... */
+      // Move the item within the list
+      // Start by making a new list without the dragged item
+      const newList = start.list.filter(
+        (_, idx) => idx !== source.index
+      )
+ 
+      // Then insert the item at the right location
+      newList.splice(destination.index, 0, start.list[source.index])
+ 
+      // Then create a new copy of the column object
+      const newCol = {
+        id: start.id,
+        list: newList
+      }
+ 
+      // Update the state
+      setColumns(state => ({ ...state, [newCol.id]: newCol }))
+      return null
     } else {
       // If start is different from end, we need to update multiple columns
       // Filter the start list like before
-      const newStartList = start.list.filter((_, idx) => idx !== source.index);
-
+      const newStartList = start.list.filter(
+        (_, idx) => idx !== source.index
+      )
+ 
       // Create a new start column
       const newStartCol = {
         id: start.id,
-        list: newStartList,
-      };
-
+        list: newStartList
+      }
+ 
       // Make a new end list array
-      const newEndList = end.list;
-
+      const newEndList = end.list
+ 
       // Insert the item into the end list
-      newEndList.splice(destination.index, 0, start.list[source.index]);
-
+      newEndList.splice(destination.index, 0, start.list[source.index])
+ 
       // Create a new end column
       const newEndCol = {
         id: end.id,
-        list: newEndList,
-      };
-
+        list: newEndList
+      }
+ 
       // Update the state
-      setColumns((state) => ({
+      setColumns(state => ({
         ...state,
         [newStartCol.id]: newStartCol,
-        [newEndCol.id]: newEndCol,
-      }));
-      return null;
+        [newEndCol.id]: newEndCol
+      }))
+      return null
     }
-  };
+   };
 
   return (
     <DragDropContext onDragEnd={onDragEnd}>
