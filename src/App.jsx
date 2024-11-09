cd9cd81ded47c0885ae573f7e670a1bba997bb6f
bhargav5454 <ultracubes7.com>
2024-11-09 12:58:23 +0530

    optimized code and add new function that create new column

--- src/App.jsx
+++ src/App.jsx
@@ -1,146 +1,127 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 import "./App.css";
 
 const App = () => {
   const initialColumns = {
-    todo: {
-      id: "todo",
-      list: ["item 1", "item 2", "item 3"],
-    },
-    doing: {
-      id: "doing",
-      list: [],
-    },
-    done: {
-      id: "done",
-      list: [],
-    },
-    reject: {
-      id: "reject",
-      list: [],
-    },
+    todo: { id: "todo", list: ["item 1", "item 2", "item 3"] },
+    doing: { id: "doing", list: [] },
+    done: { id: "done", list: [] },
+    reject: { id: "reject", list: [] },
   };
+
   const [columns, setColumns] = useState(initialColumns);
+
   const onDragEnd = ({ source, destination }) => {
-   // Make sure we have a valid destination
-   if (destination === undefined || destination === null) return null
+    if (
+      !destination ||
+      (source.droppableId === destination.droppableId &&
+        source.index === destination.index)
+    )
+      return;
 
-   // Make sure we're actually moving the item
-   if (
-     source.droppableId === destination.droppableId &&
-     destination.index === source.index
-   )
-     return null
+    const start = columns[source.droppableId];
+    const end = columns[destination.droppableId];
 
-   // Set start and end variables
-   const start = columns[source.droppableId]
-   const end = columns[destination.droppableId]
+    if (start === end) {
+      const newList = Array.from(start.list);
+      const [movedItem] = newList.splice(source.index, 1);
+      newList.splice(destination.index, 0, movedItem);
 
-   // If start is the same as end, we're in the same column
-   if (start === end) {
-     // Move the item within the list
-     // Start by making a new list without the dragged item
-     const newList = start.list.filter(
-       (_, idx) => idx !== source.index
-     )
+      setColumns((state) => ({
+        ...state,
+        [start.id]: { ...start, list: newList },
+      }));
+    } else {
+      const startList = Array.from(start.list);
+      const [movedItem] = startList.splice(source.index, 1);
+      const endList = Array.from(end.list);
+      endList.splice(destination.index, 0, movedItem);
 
-     // Then insert the item at the right location
-     newList.splice(destination.index, 0, start.list[source.index])
-
-     // Then create a new copy of the column object
-     const newCol = {
-       id: start.id,
-       list: newList
-     }
-
-     // Update the state
-     setColumns(state => ({ ...state, [newCol.id]: newCol }))
-     return null
-   } else {
-     // If start is different from end, we need to update multiple columns
-     // Filter the start list like before
-     const newStartList = start.list.filter(
-       (_, idx) => idx !== source.index
-     )
-
-     // Create a new start column
-     const newStartCol = {
-       id: start.id,
-       list: newStartList
-     }
-
-     // Make a new end list array
-     const newEndList = end.list
-
-     // Insert the item into the end list
-     newEndList.splice(destination.index, 0, start.list[source.index])
-
-     // Create a new end column
-     const newEndCol = {
-       id: end.id,
-       list: newEndList
-     }
-
-     // Update the state
-     setColumns(state => ({
-       ...state,
-       [newStartCol.id]: newStartCol,
-       [newEndCol.id]: newEndCol
-     }))
-     return null
-   }
+      setColumns((state) => ({
+        ...state,
+        [start.id]: { ...start, list: startList },
+        [end.id]: { ...end, list: endList },
+      }));
+    }
+  };
+  const name = useRef();
+  const handleColumn = () => {
+    const newColumnId = name.current.value;
+    if (newColumnId.trim() === "") return;
+    const newColumn = {
+      id: newColumnId,
+      list: [],
+    };
+    setColumns((state) => ({ ...state, [newColumn.id]: newColumn }));
+    name.current.value = "";
   };
 
   return (
-    <DragDropContext onDragEnd={onDragEnd}>
-      <div
-        
-        style={{
-          display: "grid",
-          gridTemplateColumns: "1fr 1fr 1fr",
-          margin: "24px auto",
-          width: "80%",
-          gap: "8px",
-        }}
-      >
-        {Object.values(columns).map((col) => (
-          <Droppable droppableId={col.id} key={col.id}>
-            {(provided) => (
-              <div className="text-center p-2 border rounded bg-gray-200">
-                <h2>{col.id}</h2>
-                <div
-                  key={col.id}
-                  style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    minHeight: "120px",
-                  }}
-                  {...provided.droppableProps}
-                  ref={provided.innerRef}
-                >
-                  {col.list.map((text, index) => (
-                    <Draggable draggableId={text} index={index} key={index}>
-                      {(provided) => (
-                        <div
-                          className="bg-gray-500 p-2 text-white border-2 rounded border-gray-400 m-1"
-                          ref={provided.innerRef}
-                          {...provided.draggableProps}
-                          {...provided.dragHandleProps}
-                        >
-                          {text}
-                        </div>
-                      )}
-                    </Draggable>
-                  ))}
-                  {provided.placeholder}
-                </div>
-              </div>
-            )}
-          </Droppable>
-        ))}
+    <>
+      <div className="flex justify-center items-center py-2">
+        <div className="border border-black p-2 bg-gray-100 flex flex-col items-center gap-3">
+          <input
+            type="text"
+            placeholder="add new column"
+            ref={name}
+            className="bg-gray-500 p-2 text-white border-2 rounded border-gray-400 m-1 placeholder:text-white"
+          />
+          <button
+            onClick={handleColumn}
+            className="bg-black text-white p-2 rounded"
+          >
+            submit
+          </button>
+        </div>
       </div>
-    </DragDropContext>
+      <DragDropContext onDragEnd={onDragEnd}>
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "1fr 1fr 1fr 1fr",
+            margin: "24px auto",
+            width: "80%",
+            gap: "8px",
+          }}
+        >
+          {Object.values(columns).map((col) => (
+            <Droppable key={col.id} droppableId={col.id}>
+              {(provided) => (
+                <div className="text-center p-2 border rounded bg-gray-200">
+                  <h2>{col.id}</h2>
+                  <div
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      minHeight: "120px",
+                    }}
+                    {...provided.droppableProps}
+                    ref={provided.innerRef}
+                  >
+                    {col.list.map((text, index) => (
+                      <Draggable key={text} draggableId={text} index={index}>
+                        {(provided) => (
+                          <div
+                            className="bg-gray-500 p-2 text-white border-2 rounded border-gray-400 m-1"
+                            ref={provided.innerRef}
+                            {...provided.draggableProps}
+                            {...provided.dragHandleProps}
+                          >
+                            {text}
+                          </div>
+                        )}
+                      </Draggable>
+                    ))}
+                    {provided.placeholder}
+                  </div>
+                </div>
+              )}
+            </Droppable>
+          ))}
+        </div>
+      </DragDropContext>
+    </>
   );
 };
 
