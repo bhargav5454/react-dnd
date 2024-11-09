dd83dfbe75314f17bbebfbb0273130c9f74111f7
bhargav5454 <ultracubes7.com>
2024-11-09 12:13:17 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -23,59 +23,79 @@ const App = () => {
   };
   const [columns, setColumns] = useState(initialColumns);
   const onDragEnd = ({ source, destination }) => {
-    // Make sure we have a valid destination
-    if (destination === undefined || destination === null) return null;
+   // Make sure we have a valid destination
+   if (destination === undefined || destination === null) return null
 
-    // If the source and destination columns are the same
-    // AND if the index is the same, the item isn't moving
-    if (
-      source.droppableId === destination.droppableId &&
-      destination.index === source.index
-    )
-      return null;
+   // Make sure we're actually moving the item
+   if (
+     source.droppableId === destination.droppableId &&
+     destination.index === source.index
+   )
+     return null
 
-    // Set start and end variables
-    const start = columns[source.droppableId];
-    const end = columns[destination.droppableId];
-    // If start is the same as end, we're in the same column
-    if (start === end) {
-      /* ... */
-    } else {
-      // If start is different from end, we need to update multiple columns
-      // Filter the start list like before
-      const newStartList = start.list.filter((_, idx) => idx !== source.index);
+   // Set start and end variables
+   const start = columns[source.droppableId]
+   const end = columns[destination.droppableId]
 
-      // Create a new start column
-      const newStartCol = {
-        id: start.id,
-        list: newStartList,
-      };
+   // If start is the same as end, we're in the same column
+   if (start === end) {
+     // Move the item within the list
+     // Start by making a new list without the dragged item
+     const newList = start.list.filter(
+       (_, idx) => idx !== source.index
+     )
 
-      // Make a new end list array
-      const newEndList = end.list;
+     // Then insert the item at the right location
+     newList.splice(destination.index, 0, start.list[source.index])
 
-      // Insert the item into the end list
-      newEndList.splice(destination.index, 0, start.list[source.index]);
+     // Then create a new copy of the column object
+     const newCol = {
+       id: start.id,
+       list: newList
+     }
 
-      // Create a new end column
-      const newEndCol = {
-        id: end.id,
-        list: newEndList,
-      };
+     // Update the state
+     setColumns(state => ({ ...state, [newCol.id]: newCol }))
+     return null
+   } else {
+     // If start is different from end, we need to update multiple columns
+     // Filter the start list like before
+     const newStartList = start.list.filter(
+       (_, idx) => idx !== source.index
+     )
 
-      // Update the state
-      setColumns((state) => ({
-        ...state,
-        [newStartCol.id]: newStartCol,
-        [newEndCol.id]: newEndCol,
-      }));
-      return null;
-    }
+     // Create a new start column
+     const newStartCol = {
+       id: start.id,
+       list: newStartList
+     }
+
+     // Make a new end list array
+     const newEndList = end.list
+
+     // Insert the item into the end list
+     newEndList.splice(destination.index, 0, start.list[source.index])
+
+     // Create a new end column
+     const newEndCol = {
+       id: end.id,
+       list: newEndList
+     }
+
+     // Update the state
+     setColumns(state => ({
+       ...state,
+       [newStartCol.id]: newStartCol,
+       [newEndCol.id]: newEndCol
+     }))
+     return null
+   }
   };
 
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <div
+        
         style={{
           display: "grid",
           gridTemplateColumns: "1fr 1fr 1fr",
@@ -85,7 +105,7 @@ const App = () => {
         }}
       >
         {Object.values(columns).map((col) => (
-          <Droppable droppableId={col.id}>
+          <Droppable droppableId={col.id} key={col.id}>
             {(provided) => (
               <div className="text-center p-2 border rounded bg-gray-200">
                 <h2>{col.id}</h2>
